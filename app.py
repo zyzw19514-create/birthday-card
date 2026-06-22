26447520b8954fd56e301bbe70618ba8911895bc
zyzw19514-create <zyzw19514@gmail.com>
2026-06-22 10:16:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 import streamlit as st
 
-st.set_page_config(
-page_title=“Happy Birthday Janjon ❤️”,
-page_icon=“❤️”,
-layout=“centered”
-)
-
+st.set_page_config(page_title="Happy Birthday Janjon", page_icon="🐣", layout="centered")
 st.markdown(”””
 
 <style>
